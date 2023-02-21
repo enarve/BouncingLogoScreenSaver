63381843d9e77f9d5feabcce893611a7bc2f5c8d
Nikita Vlaskin <sinezeleuny@gmail.com>
2023-02-21 10:10:12 +0400

    Initial Commit

---
+++ BouncingLogoScreenSaver/BouncingLogoScreenSaverView.h
@@ -0,0 +1,14 @@
+//
+//
+// BouncingLogoScreenSaverView.h
+// BouncingLogoScreenSaver
+//
+// Created by sinezeleuny on 21.02.2023
+//
+        
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BouncingLogoScreenSaverView : ScreenSaverView
+
+@end
---
+++ BouncingLogoScreenSaver/BouncingLogoScreenSaverView.m
@@ -0,0 +1,53 @@
+//
+//
+// BouncingLogoScreenSaverView.m
+// BouncingLogoScreenSaver
+//
+// Created by sinezeleuny on 21.02.2023
+//
+        
+
+#import "BouncingLogoScreenSaverView.h"
+
+@implementation BouncingLogoScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
